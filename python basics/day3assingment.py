38513236044428f29cd3231468609d9456ac9629
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-03-25 01:44:49 +0530

    string and operations

--- python basics/day3assingment.py
+++ python basics/day3assingment.py
@@ -34,10 +34,92 @@
 # And to remove the right most digit from a number, we can do N = N / 10.]
 
 
-def print_digits(n):
-    while n > 0:
-        digit = n % 10
-        print(digit)
-        n = n //10
+# def print_digits(n):
+#     while n > 0:
+#         digit = n % 10
+#         print(digit)
+#         n = n //10
 
-print_digits(312)
+# print_digits(312)
+
+# Q4. Write a function to return the count the number of digits in a number n 
+
+
+# def count_digits(n):
+#     count = 0
+    
+#     while n > 0:
+#         n = n // 10
+#         count += 1
+        
+#     return count
+# print(count_digits(312))
+
+# Write a function to return the sum of digits of a number n 
+
+# def sum_digits(n):
+#     addition = 0
+
+#     while n > 0:
+#         digit = n%10
+#         addition+=digit
+#         n = n//10
+#     return addition
+
+# print(sum_digits(123))
+
+# Q6. Write a program to print all numbers from 1 to 100 that are divisible by both 3
+# and 5.
+
+# count = 0
+# for i in range(1,100):
+#      if i%3==0 or i%5==0:
+#         print(i)
+
+# Q7. Design a program to continuously input a number from user & print if it is
+# positive or negative until the user enters “Quit”.
+
+# def check_number():
+#     while True:
+#         user_input = input("Enter a number (or Quit to exit): ")
+        
+#         if user_input == "Quit":
+#             print("Program ended")
+#             break
+        
+#         num = int(user_input)
+        
+#         if num > 0:
+#             print("Positive")
+#         elif num < 0:
+#             print("Negative")
+#         else:
+#             print("Zero")
+
+# check_number()
+
+# Q8. Letʼs create a Simple Calculator that performs arithmetic operations. Create
+# a function calculator(a, b, operation) that performs addition, subtraction,
+# multiplication, or division based on the operation parameter.
+# [ parameter can have values +,-,*,/]
+       
+
+def calculator(a,b,operation):
+    ans = 0
+    if operation == '+':
+        ans = a+b
+    elif operation == '-':
+        ans = a-b
+    elif operation == '*':
+        ans = a*b
+    else :
+        ans = a/b   
+    return ans 
+
+print(calculator(20,10,'*'))
+
+
+        
+        
+
+
---
+++ python basics/string_list_tuple_dictionary_set/STRING.py
@@ -0,0 +1,36 @@
+# word = "python"
+# print(len(word))
+
+# word1 = "i love"
+# word2 = "java"
+# sentence = word1+" "+word2
+# print(sentence)
+
+# word3 = "Faizan"
+# print(word3[3])
+
+# slicing string (string ke tukde karke sub string bnana)
+
+# syntax 
+# str = python (lets say we have to extract th as a substring)
+
+#=> str[starting index : ending index(not including) +1]
+
+# str = "python"
+# print(str[2:4])
+
+# str1 = "i am faizan"
+# print(str1[:4])
+# print(str1[0:len(str1)])
+
+#string formatting
+
+a = 5
+b = 10
+sum = a+b
+
+# normal formatting 
+print ("sum of {} & {} is {}".format(a,b,sum))
+
+#example 2 
+print("language is {}".format("python"))
