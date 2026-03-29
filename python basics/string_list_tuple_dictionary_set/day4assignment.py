9424b63ad40b899b3a0a84cfca3e4194c8290f3c
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-03-30 00:59:10 +0530

    updated

--- python basics/string_list_tuple_dictionary_set/day4assignment.py
+++ python basics/string_list_tuple_dictionary_set/day4assignment.py
@@ -25,18 +25,39 @@
 # print(sum)
 
 
-s1= input("enter numbers : ")
-list1 = []
-s1.split()
-for x in s1:
-    list1.append(int(x))
-s2 = input ("enter numbers : ")
-list2=[]
-s2.split()
-for x in s2:
-    list2.append(int(x))
+# s1= input("enter numbers : ")
+# list1 = []
+# s1.split()
+# for x in s1:
+#     list1.append(int(x))
+# s2 = input ("enter numbers : ")
+# list2=[]
+# s2.split()
+# for x in s2:
+#     list2.append(int(x))
 
-result = list1+list2
-result.sort()
-print(result)
+# result = list1+list2
+# result.sort()
+# print(result)
 
+
+# Given a tuple of integers, create:
+# • A tuple of all even numbers
+# • A tuple of all odd numbers
+
+# tup = (1,2,3,4,5,6,7,8,9,10)
+
+# even = []
+# odd = []
+
+# for i in tup :
+#     if(i%2==0):
+#         even.append(i)
+#     else:
+#         odd.append(i)
+
+# even_tuple = (tuple(even))
+# odd_tuple = tuple(odd)
+
+# print(even_tuple)
+# print(odd_tuple)
