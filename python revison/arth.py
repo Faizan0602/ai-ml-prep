6d93497673f1d791132977500fb4f65ae53f8847
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-04-09 01:08:31 +0530

    more revision

---
+++ python revison/arth.py
@@ -0,0 +1,33 @@
+#arthmetic 
+
+# a = 10
+# b = 5
+
+# print("sum is : " , a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+
+# #relational 
+
+# print(a>b)
+# print(a<b)
+# print(a>=b)
+# print(a<=b)
+# print(a!=b)
+# print(a==b)
+
+# #assignment
+# c = 10
+# c+=5
+# print(c)
+
+#logical
+var = False
+print(not var) #true , makes true false and false true 
+
+print((3<8) and (2<5)) # only returns true if both are true if a single is false answer is false 
+
+print((3<2) or (5<1)) #only returns false if both values are false otherwise true 
+
---
+++ python revison/userinp.py
@@ -0,0 +1,12 @@
+# username = input("enter user name :")
+# print("welcome" ,username)
+
+#sum of two numbers :
+
+num1 = int(input("enter first number : " )) #we do type casting cause input takes value as string by default 
+num2 = int(input("enter second number : "))
+sum = num1+num2
+print(sum)
+
+avg = (num1+num2)/2
+print(avg)
