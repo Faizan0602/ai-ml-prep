5dea787f14903c3b9d1036de60d6bce69924fa69
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-04-09 01:51:44 +0530

    revision

---
+++ python revison/revday3.py
@@ -0,0 +1,65 @@
+# conditional statements (if , elif, else) , if a particular condition is true then only program should execute
+
+# check if user can vote or not 
+
+# age = int(input("enter user age : "))
+
+# if age>=18:
+#     print("can vote")
+# else:
+#     print("not adult")
+
+#traffic color check 
+# color = input("enter the color : ")
+
+# if color  == "red":
+#     print("stop")
+# elif color == "yellow" :
+#     print("look")
+# elif color == "green":
+#     print("go")
+# else : 
+#     print("wrong color entered") 
+
+# check user age and generation 
+
+# age = int(input("enter age : "))
+
+# if (age<13):
+#     print("child")
+# elif(age>=13 and age<18):
+#     print("teen")
+# else:
+#     print("adult")
+
+#admin password
+
+# username = input("enter username : ")
+# password = (input("enter password :"))
+
+# if(username=="admin" and password == "pass"):
+#     print("login sucessfull")
+# elif(username == "admin" and password!="pass"):
+#     print("wrong password")
+# else:
+#     print("wrong username")
+
+# n is multiple of 5 or not
+
+# n = int(input("enter number : "))
+
+# if (n%5==0):
+#     print("n is a multiple of 5")
+# else:
+#     print("not a multiple of 5")
+
+
+#ODD EVEN 
+
+n = int(input("enter the number to be checked : "))
+if(n%2==0):
+    print("even")
+else:
+    print("odd")
+
+
