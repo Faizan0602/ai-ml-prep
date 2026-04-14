b4869c6088927e22517abbc1b483da9cd540fda2
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-04-14 16:13:45 +0530

    string rev

--- python revison/assignmentprobrev.py
+++ python revison/assignmentprobrev.py
@@ -65,10 +65,12 @@
 # Q6. Write a program to print all numbers from 1 to 100 that are divisible by both 3
 # 3 and 5.
 
-for i in range(1,101):
-    if i%3==0 and i%5==0:
-        print(i)
-        i+=1
+# for i in range(1,101):
+#     if i%3==0 and i%5==0:
+#         print(i)
+#         i+=1
+
+
 
 
 
---
+++ python revison/day4rev.py
@@ -0,0 +1,58 @@
+# #string in python (sequence of characters)
+# word1 = "i love"
+# word2 = "python" #(sequence of characters : 'p','y','t','h','o','n')
+
+# print(len(word1))
+# sentence = word1+" " +word2 #(concatenation)
+# print(sentence)
+
+# #indexing 
+
+# word3 = "faizan"   #accessing a particular charac of string
+# print(word3[2])
+
+# for ch in word3:   #for loops to print characters of string
+#     print(ch)
+
+# # word[2] = 'z'   (not possible to change , strings are immutable)
+
+
+# slicing in strings - string ke tukde karke sub string banana 
+
+#str[starting index : ending index(not included)]
+
+# word = "python" # we have to print only "th"
+# print(word[2:4]) #-output 'th'
+
+#string formatting 
+
+#1 .format()
+
+#normal formatting
+a = 5
+b = 10
+sum = a+b
+# print("sum of {} and {} is {}".format(a,b,sum))
+# print("language is {}".format("python"))
+
+
+#index based formatting
+# print("sum of {1} and {0} is {2}".format(a,b,sum))
+
+#value based formatting - we can assign values to variable 
+
+print("value of vars {a} and {b} is ".format(a=2,b=3))
+
+
+#f-strings()
+
+a =5
+b=10
+print(f"sum of {a} and {b} is {a+b}")
+
+
+
+
+
+
+
