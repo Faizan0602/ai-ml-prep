d3faff270cec56d70b40a97180b6dd936cb58bda
Faizan Ahmad <faizan.ahmad12877@gmail.com>
2026-08-10 18:35:00 +0530

    learning req module in python

--- OOPS/oop.py
+++ OOPS/oop.py
@@ -176,18 +176,17 @@
 # Print the area of r1
 
 
-class Rectangle : 
+# class Rectangle : 
     
-    def __init__(self,width,height):
-        self.width=width
-        self.height=height
+#     def __init__(self,width,height):
+#         self.width=width
+#         self.height=height
     
-    def area(self):
-        return self.width*self.height
+#     def area(self):
+#         return self.width*self.height
 
-r1 = Rectangle(5,3)
-print(r1.area())
+# r1 = Rectangle(5,3)
+# print(r1.area())
 
         
             
-        
